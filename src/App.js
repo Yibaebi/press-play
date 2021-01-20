d04e9b188ae01789e8d4a847c5bf5cc3c4091757
Yibaebi <elliot.yibaebi@yahoo.com>
2021-01-20 09:43:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { logo } from "./assets";
+import { Button } from "./components";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import eli from "./images/05D10002-48EC-4D1E-BA3F-8957EC3D2651L0001.jpeg";
+import logo from "../logo.svg";
+
+export { eli, logo };
