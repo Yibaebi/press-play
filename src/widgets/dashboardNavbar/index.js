03c6d3d8eb9d5b97a1d6c7f8c81f617f68a2f1e0
ogubuike alex <ogubuikealex@gmail.com>
2021-02-10 17:33:52 +0100

    Creating the Search service For the Press Play app

--- src/widgets/dashboardNavbar/index.js
+++ src/widgets/dashboardNavbar/index.js
@@ -1,42 +1,51 @@
-import React from "react";
-import { hamburgerUnclicked, primaryLogo, searchIcon } from "../../assets";
+import React, { Component } from "react";
+import {searchIcon } from "../../assets";
 import profileImage from "../../assets/images/05D10002-48EC-4D1E-BA3F-8957EC3D2651L0001.jpeg";
 import { Link } from "react-router-dom";
 import "./dashboardNavbar.css";
+import  { searchService } from "../../api/auth";
 
-function DashboardNavBar({ handleModalOpen, user }) {
-  return (
-    <React.Fragment>
-      <div className="logo-container">
-        <img
-          onClick={handleModalOpen}
-          src={hamburgerUnclicked}
-          alt="pressplay logo"
-        />
+class DashboardNavBar extends Component {
+  state = { searchQuery: "" };
 
-        <Link to="/dashboard/home">
-          <img
-            src={primaryLogo}
-            className="pressplay-logo"
-            alt="pressplay logo"
-          />
-        </Link>
-      </div>
-      <div className="dashboard-search-profile">
-        <div htmlFor="search" className="dashboard-nav-search">
-          {searchIcon()}
-          <input type="search" />
-        </div>
+  handleInputChange = (event) => {
+    this.setState({
+      searchQuery: event.target.value,
+    });
+  };
 
-        <div className="dashboard-profile">
-          <p>{user}</p>
-          <div className="profile-avatar">
-            <img src={profileImage} alt="profile" />
+  handleFormSubmit = async (event) => {
+    try {
+      const searchResponse = await searchService(this.state.searchQuery);
+      console.log(searchResponse.data);
+    } catch (error) {}
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="dashboard-search-profile">
+          <div htmlFor="search" className="dashboard-nav-search">
+            {searchIcon()}
+            <form onSubmit={this.handleFormSubmit}>
+              <input
+                type="search"
+                value={this.state.searchQuery}
+                onChange={this.handleInputChange}
+              />
+            </form>
+          </div>
+
+          <div className="dashboard-profile">
+            <p>{this.props.user}</p>
+            <div className="profile-avatar">
+              <img src={profileImage} alt="profile" />
+            </div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export { DashboardNavBar };
